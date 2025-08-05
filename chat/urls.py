6d17d525b7cb8ade5e98cf7f0e169c3e95f4bfb9
niloofar-samei <niloofar.samei@gmail.com>
2025-08-06 00:42:05 +0100

    Set urls.

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, ConversationListCreateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "conversations/",
+        ConversationListCreateView.as_view(),
+        name="conversation-list-create",
+    ),
 ]
