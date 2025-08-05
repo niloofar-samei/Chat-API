b9da0d62543dc8c0802f69c003053aab2ed01414
niloofar-samei <niloofar.samei@gmail.com>
2025-08-05 23:53:46 +0100

    Fix typo.

--- chat/serializers.py
+++ chat/serializers.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializer.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
         fields = ["username", "password", "email"]
 
     def create(self, validated_data):
-        retrun User.objects.create_user(**validated_data)
+        return User.objects.create_user(**validated_data)
