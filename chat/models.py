c26f51226e4479838f504101b495c1fccda81af2
niloofar-samei <niloofar.samei@gmail.com>
2025-08-06 00:11:51 +0100

    Create Conversation model.

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Conversation(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    participants = models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.name:
+            return self.name
+        return f"Conversation {self.id}"
