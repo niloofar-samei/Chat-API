f0b85352e7c77d915085e7d36c90ca4a50d04a0d
niloofar-samei <niloofar.samei@gmail.com>
2025-08-05 23:46:57 +0100

    Create UserRegistrationView.

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
