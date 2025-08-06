e9d69edb2b32ca4148676e1213c4b49fa087310e
niloofar-samei <niloofar.samei@gmail.com>
2025-08-06 01:03:38 +0100

    Create Message model.

--- chat/models.py
+++ chat/models.py
@@ -11,3 +11,16 @@ class Conversation(models.Model):
         if self.name:
             return self.name
         return f"Conversation {self.id}"
+
+
+class Message(models.Model):
+    Conversation = models.ForeignKey(
+        "Conversation", on_delete=models.CASCADE, related_name="messages"
+    )
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.sender.username}: {self.text[:30]}"
