b44e69438d0580248ed6c4e860baaadb8107028c
niloofar-samei <niloofar.samei@gmail.com>
2025-08-06 01:52:12 +0100

    Add id to UserRegistrationSerializer.

--- chat/serializers.py
+++ chat/serializers.py
@@ -8,7 +8,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["username", "password", "email"]
+        fields = ["id", "username", "password", "email"]
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
