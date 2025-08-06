210bba2ef1a697a83a3dc0fcb530bc3e486c105d
niloofar-samei <niloofar.samei@gmail.com>
2025-08-06 01:17:01 +0100

    Create MessageListCreateView.

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,12 @@
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
-from .serializers import UserRegistrationSerializer, ConversationSerializer
-from .models import Conversation
+from .serializers import (
+    UserRegistrationSerializer,
+    ConversationSerializer,
+    MessageSerializer,
+)
+from .models import Conversation, Message
 
 
 class UserRegistrationView(generics.CreateAPIView):
@@ -20,3 +24,16 @@ class ConversationListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         conversation = serializer.save()
         conversation.participants.add(self.request.user)
+
+
+class MessageListCreateView(generics.ListCreateAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        conversation_id = self.kwargs["conversation_id"]
+        return Message.objects.filter(conversation__id=conversation_id)
+
+    def perform_create(self, serializer):
+        conversation_id = self.kwargs["conversation_id"]
+        serializer.save(sender=self.request.user, conversation_id=conversation_id)
