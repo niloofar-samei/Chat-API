694db02d05c693ddec37fdd1a42e24b34febb624
niloofar-samei <niloofar.samei@gmail.com>
2025-08-12 18:43:24 +0100

    Add lifetime to access and refresh tokens.

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,4 +138,9 @@ REST_FRAMEWORK = {
     ),
     # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     # "PAGE_SIZE": 5,
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),  # e.g., 15 minutes
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),  # e.g., 7 days.
 }
